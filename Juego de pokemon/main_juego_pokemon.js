4ddfc8183ef114ccc2283a5547d4da014d472c13
Jorge Piñeiro <jorge.p.salvagno@gmail.com>
2024-01-24 13:50:28 -0300

    Pokemon juego

--- Juego de pokemon/main_juego_pokemon.js
+++ Juego de pokemon/main_juego_pokemon.js
@@ -1,5 +1,9 @@
 import { login } from "../Juego de Memoria/js/usuario.js"
 
+let fallos = 0
+let pokemonAcertados = 0
+
+
 const botonJuegoMemoria = document.querySelector('#juegoMemoria')
 botonJuegoMemoria.addEventListener('click', ()=>{
   event.preventDefault()
@@ -8,13 +12,21 @@ botonJuegoMemoria.addEventListener('click', ()=>{
 })
 
 
+document.querySelector('#respuesta').addEventListener('keydown', ()=> {
+  if (event.key === 'Enter'){
+    verificarRespuesta()
+    document.querySelector('#respuesta').value = ''
+  }
+
+})
 
 
+// Utilizamos la Api, pokeapi.
 const urlBase = 'https://pokeapi.co/api/v2/pokemon/';
   let pokemonActual = {};
 
   // Función para obtener un Pokémon aleatorio de la primera generación (1-151)
-  async function obtenerPokemonAleatorio() {
+  async function obtenerPokemonPrimerGen() {
     const numeroAleatorio = Math.floor(Math.random() * 151) + 1; // Primera generación
     const urlPokemon = `${urlBase}${numeroAleatorio}`;
 
@@ -42,15 +54,21 @@ const urlBase = 'https://pokeapi.co/api/v2/pokemon/';
   function verificarRespuesta() {
     const respuestaUsuario = document.getElementById('respuesta').value.toLowerCase();
     if (respuestaUsuario === pokemonActual.nombre) {
-      document.getElementById('mensaje').innerText = '¡Correcto! ¡Ese es el Pokémon!';
-      obtenerPokemonAleatorio(); // Obtener otro Pokémon después de acertar
+      pokemonAcertados++
+      document.getElementById('mensaje').innerText = '¡Correcto! ¡Ese es el Pokémon!'
+      obtenerPokemonPrimerGen(); // Obtener otro Pokémon después de acertar
     } else {
       document.getElementById('mensaje').innerText = 'Incorrecto. ¡Sigue intentando!';
+      fallos ++
     }
+
+    const resultados = document.querySelector('#resultados')
+    resultados.innerText = `Pokémon acertados: ${pokemonAcertados}/151
+                        Intentos: ${fallos}`
   }
 
   // Iniciar el juego al cargar la página
-  window.onload = obtenerPokemonAleatorio;
+  window.onload = obtenerPokemonPrimerGen;
 
 
 
