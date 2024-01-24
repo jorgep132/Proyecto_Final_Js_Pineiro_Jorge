3dc7530da6850db6b02781ef945656a1de9f2f4c
Jorge Piñeiro <jorge.p.salvagno@gmail.com>
2024-01-24 14:31:39 -0300

    Creando juego Pokemon

--- Juego de pokemon/main_juego_pokemon.js
+++ Juego de pokemon/main_juego_pokemon.js
@@ -2,7 +2,8 @@ import { login } from "../Juego de Memoria/js/usuario.js"
 
 let fallos = 0
 let pokemonAcertados = 0
-
+let generacion = 151
+let listPoke = []
 
 const botonJuegoMemoria = document.querySelector('#juegoMemoria')
 botonJuegoMemoria.addEventListener('click', ()=>{
@@ -24,22 +25,33 @@ document.querySelector('#respuesta').addEventListener('keydown', ()=> {
 // Utilizamos la Api, pokeapi.
 const urlBase = 'https://pokeapi.co/api/v2/pokemon/';
   let pokemonActual = {};
+  let pokemonAdivinados = new Set()
 
   // Función para obtener un Pokémon aleatorio de la primera generación (1-151)
-  async function obtenerPokemonPrimerGen() {
-    const numeroAleatorio = Math.floor(Math.random() * 151) + 1; // Primera generación
-    const urlPokemon = `${urlBase}${numeroAleatorio}`;
+  async function obtenerPokemon(generacion) {
+    
+    let numeroAleatorio
+    do {
+        numeroAleatorio = Math.floor(Math.random() * generacion) + 1
+    } while (pokemonAdivinados.has(numeroAleatorio))
 
+    const urlPokemon = `${urlBase}${numeroAleatorio}`
+    
     try {
       const respuesta = await fetch(urlPokemon);
       const datos = await respuesta.json();
+      const id = datos.id || obtenerPokemonIdDesdeUrl(urlPokemon);
+
       pokemonActual = {
         nombre: datos.name,
-        imagen: datos.sprites.front_default
+        imagen: datos.sprites.front_default,
+        id: id
       };
+
       mostrarPokemon();
+
     } catch (error) {
-      console.error('Error al obtener el Pokémon', error);
+      console.error('Missing', error);
     }
   }
 
@@ -48,27 +60,49 @@ const urlBase = 'https://pokeapi.co/api/v2/pokemon/';
     document.getElementById('mensaje').innerText = '';
     document.getElementById('imagenPokemon').src = pokemonActual.imagen;
     document.getElementById('respuesta').value = '';
+
+    pokemonAdivinados.add(pokemonActual.id)
+    
   }
+
+  function obtenerPokemonIdDesdeUrl(url) {
+    // La URL tiene el formato 'https://pokeapi.co/api/v2/pokemon/{id}/'
+    const idMatch = url.match(/\/(\d+)\/$/);
+    if (idMatch && idMatch[1]) {
+      return parseInt(idMatch[1], 10);
+    }
+    return null;
+  }
+
+
 
   // Función para verificar la respuesta del usuario
   function verificarRespuesta() {
     const respuestaUsuario = document.getElementById('respuesta').value.toLowerCase();
-    if (respuestaUsuario === pokemonActual.nombre) {
+    if (respuestaUsuario === pokemonActual.nombre || pokemonActual.nombre.includes("nidoran")) {
       pokemonAcertados++
       document.getElementById('mensaje').innerText = '¡Correcto! ¡Ese es el Pokémon!'
-      obtenerPokemonPrimerGen(); // Obtener otro Pokémon después de acertar
+      obtenerPokemon(generacion); // Obtener otro Pokémon después de acertar
     } else {
       document.getElementById('mensaje').innerText = 'Incorrecto. ¡Sigue intentando!';
       fallos ++
+      obtenerPokemon(generacion)
     }
 
+    listPoke.push(pokemonActual.nombre)
+    
     const resultados = document.querySelector('#resultados')
     resultados.innerText = `Pokémon acertados: ${pokemonAcertados}/151
                         Intentos: ${fallos}`
+    
+    const listaPokemon = document.querySelector('#listaPokemon')
+    listaPokemon.innerText = `${listPoke.join(', ')}`
+    
   }
 
   // Iniciar el juego al cargar la página
-  window.onload = obtenerPokemonPrimerGen;
+  window.onload = obtenerPokemon(generacion)
+ 
 
 
 
